9806693351bd45849ead81d02d25229458a98bcd
Maximillian Rubensson <maximoff87@gmail.com>
2025-04-28 18:38:07 +0200

    refactor Main

--- sortingrunner/src/main/java/se/iths/java24/sortingrunner/main/Main.java
+++ sortingrunner/src/main/java/se/iths/java24/sortingrunner/main/Main.java
@@ -1,22 +1,40 @@
 package se.iths.java24.sortingrunner.main;
 
 import se.iths.java24.service.SortingService;
-
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please enter the size of the array: ");
-        int size = scanner.nextInt();
-        int[] array = new int[size];
+        int size = 0;
+        while (true) {
+            try {
+                System.out.print("""
+                        Hello!
+                        Please enter the size of the array:\s""");
+                size = scanner.nextInt();
+                if (size > 0) break;
+                System.out.println("\nSize must be a positive number.");
+            } catch (InputMismatchException e) {
+                System.out.println("\nInvalid input! Please enter a valid number.");
+                scanner.next();
+            }
+        }
 
-        System.out.println("Please enter numbers to the array: ");
+        int[] array = new int[size];
+        System.out.println("\nPlease enter " + size + " numbers to the array");
         for (int i = 0; i < size; i++) {
-            System.out.println("Number: ");
-            array[i] = scanner.nextInt();
+            while (true) {
+                try {
+                    System.out.print("Number " + (i + 1) + ": ");
+                    array[i] = scanner.nextInt();
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("\nInvalid number! Please enter a valid integer.");
+                    scanner.next();
+                }
+            }
         }
 
         ServiceLoader<SortingService> loader = ServiceLoader.load(SortingService.class);
@@ -25,27 +43,34 @@ public class Main {
             services.add(service);
         }
 
-        System.out.println("\nPlease choose sorting algorithm: ");
+        System.out.println("\nPlease choose a sorting algorithm");
         for (int i = 0; i < services.size(); i++) {
             System.out.println((i + 1) + ". " + services.get(i).getClass().getSimpleName());
         }
 
-        System.out.println("\nYour Choice: ");
-        int choice = scanner.nextInt();
-
-        if (choice < 1 || choice > services.size()) {
-            System.out.println("Invalid choice.");
-        } else {
-            SortingService selectedService = services.get(choice - 1);
-            System.out.println("\nYou choose " + selectedService.getClass().getSimpleName());
-            int[] sortedArray = selectedService.execute(Arrays.copyOf(array, array.length));
-
-            System.out.println("Sorted Array: ");
-            for (int num : sortedArray) {
-                System.out.print(num + " ");
+        int choice = -1;
+        while (true) {
+            try {
+                System.out.println("\nYour choice: ");
+                choice = scanner.nextInt();
+                if (choice >= 1 && choice <= services.size()) break;
+                System.out.print("\nInvalid choice! Please enter a number between 1 and " + services.size());
+            } catch (InputMismatchException e) {
+                System.out.print("\nInvalid input! Please enter a valid number.");
+                scanner.next();
             }
-            System.out.println();
         }
+
+        SortingService selectedService = services.get(choice - 1);
+        System.out.print("\nYou chose: " + selectedService.getClass().getSimpleName());
+        int[] sortedArray = selectedService.execute(Arrays.copyOf(array, array.length));
+
+        // Visa sorterad array
+        System.out.print("\nSorted Array: ");
+        for (int num : sortedArray) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
 
         scanner.close();
     }
