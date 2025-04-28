4338e422ad46d6d9241b153f060a99924528908a
Maximillian Rubensson <maximoff87@gmail.com>
2025-04-28 12:53:37 +0200

    add BubbleSort.java

---
+++ bubblesorter/src/main/java/se/iths/java24/bubblesorter/BubbleSort.java
@@ -0,0 +1,18 @@
+package se.iths.java24.bubblesorter;
+
+public class BubbleSort {
+
+    public int[] execute(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
