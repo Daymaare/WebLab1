419665441373a50f7a4db230fa31f033d40ae962
Maximillian Rubensson <maximoff87@gmail.com>
2025-04-28 17:50:41 +0200

    change in QuickSort.java

--- quicksorter/src/main/java/se/iths/java24/quicksorter/QuickSort.java
+++ quicksorter/src/main/java/se/iths/java24/quicksorter/QuickSort.java
@@ -1,7 +1,6 @@
 package se.iths.java24.quicksorter;
 
 import se.iths.java24.service.SortingService;
-import static java.util.Arrays.sort;
 
 public class QuickSort implements SortingService {
 
@@ -15,29 +14,32 @@ public class QuickSort implements SortingService {
         if (low < high) {
             int partitionIndex = partition(array, low, high);
 
-            sort(array, 0, partitionIndex-1);
-            sort(array, partitionIndex+1, high);
+            // Correct recursive calls for left and right partitions
+            quickSort(array, low, partitionIndex - 1);
+            quickSort(array, partitionIndex + 1, high);
         }
     }
 
-    private int partition(int[] array,int low, int high) {
+    private int partition(int[] array, int low, int high) {
         int pivot = array[high];
-        int i = (low -1);
+        int i = low - 1;
 
-        for (int j = 0; j < high; j++) {
+        // Correct loop bounds: j from low to high-1
+        for (int j = low; j < high; j++) {
             if (array[j] <= pivot) {
                 i++;
-
-                int swapTemp = array[i];
+                // Swap array[i] and array[j]
+                int temp = array[i];
                 array[i] = array[j];
-                array[j] = swapTemp;
+                array[j] = temp;
             }
         }
 
-        int swapTemp = array[i+1];
-        array[i+1] = array[high];
-        array[high] = swapTemp;
+        // Swap array[i+1] and array[high] (pivot)
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
 
-        return i+1;
+        return i + 1;
     }
 }
