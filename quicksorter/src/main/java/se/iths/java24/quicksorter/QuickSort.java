140da1082b6b635312080457d916e208ee89223f
Maximillian Rubensson <maximoff87@gmail.com>
2025-04-28 17:51:17 +0200

    change in QuickSort.java

--- quicksorter/src/main/java/se/iths/java24/quicksorter/QuickSort.java
+++ quicksorter/src/main/java/se/iths/java24/quicksorter/QuickSort.java
@@ -14,7 +14,6 @@ public class QuickSort implements SortingService {
         if (low < high) {
             int partitionIndex = partition(array, low, high);
 
-            // Correct recursive calls for left and right partitions
             quickSort(array, low, partitionIndex - 1);
             quickSort(array, partitionIndex + 1, high);
         }
@@ -24,18 +23,15 @@ public class QuickSort implements SortingService {
         int pivot = array[high];
         int i = low - 1;
 
-        // Correct loop bounds: j from low to high-1
         for (int j = low; j < high; j++) {
             if (array[j] <= pivot) {
                 i++;
-                // Swap array[i] and array[j]
                 int temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;
             }
         }
-
-        // Swap array[i+1] and array[high] (pivot)
+        
         int temp = array[i + 1];
         array[i + 1] = array[high];
         array[high] = temp;
