6cc4e2fede72cb57b5ad7e7f69058a7705dd29dd
Maximillian Rubensson <maximoff87@gmail.com>
2025-05-05 10:29:48 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,15 @@
 
 set -e
 
-MODULE_PATH="service/target/service-1.0-SNAPSHOT.jar:bubblesorter/target/bubblesorter-1.0-SNAPSHOT.jar:quicksorter/target/quicksorter-1.0-SNAPSHOT.jar:sortingrunner/target/sortingrunner-1.0-SNAPSHOT.jar"
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" || "$OSTYPE" == "cygwin" ]]; then
+  SEP=";"
+else
+  SEP=":"
+fi
+
+MODULE_PATH="service/target/service-1.0-SNAPSHOT.jar${SEP}bubblesorter/target/bubblesorter-1.0-SNAPSHOT.jar${SEP}quicksorter/target/quicksorter-1.0-SNAPSHOT.jar${SEP}sortingrunner/target/sortingrunner-1.0-SNAPSHOT.jar"
 
 MAIN_MODULE="se.iths.java.sortingrunner"
 MAIN_CLASS="se.iths.java24.sortingrunner.main.Main"
 
-java --module-path "$MODULE_PATH" -m "$MAIN_MODULE/$MAIN_CLASS"
+java --module-path "$MODULE_PATH" -m "$MAIN_MODULE/$MAIN_CLASS" "$MAIN_MODULE/$MAIN_CLASS"
